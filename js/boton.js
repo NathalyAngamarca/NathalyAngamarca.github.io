e7b96d76dafd2a593fe0206c745408faf191c804
Nathaly Angamarca <148395616+NathalyAngamarca@users.noreply.github.com>
2023-12-26 23:25:07 -0500

    Add files via upload

--- js/boton.js
+++ js/boton.js
@@ -1,1 +1,11 @@
+function redirectToInformation() {
+    window.location.href = 'information.html';
+}
 
+function redirectToLogin() {
+    window.location.href = 'login.html';
+}
+
+function redirectToRegister() {
+    window.location.href = 'register.html';
+}
