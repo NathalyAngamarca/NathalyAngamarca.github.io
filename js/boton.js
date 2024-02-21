47486d1bf26f14cac943469a0bc9b13526f5a07c
NathalyAngamarca <nathaly.angamarca@unl.edu.ec>
2024-02-21 16:11:17 -0500

    lol

---
+++ js/boton.js
@@ -0,0 +1,11 @@
+function redirectToInformation() {
+    window.location.href = 'information.html';
+}
+
+function redirectToLogin() {
+    window.location.href = 'login.html';
+}
+
+function redirectToRegister() {
+    window.location.href = 'register.html';
+}
